715ad342e6c05852c98815786774ea699707119f
xaviermaisel <62498193+xaviermaisel@users.noreply.github.com>
2020-03-22 04:05:57 -0400

    Add files via upload

---
+++ RockPaperSciccors/src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,74 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * @author -- Xavier Maisel / CSCI 2467 / 03/22/2020
+ */
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * @author – Xavier Maisel
+     * @param – String pick, the value which the human typed
+     * @return – boolean, returns true if the picked value is a valid value in (ROCK, PAPER, SCISSORS, LIZARD, SPOCK)
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * @author – Xavier Maisel
+     * @return – String, Generates and returns a random string in (ROCK, PAPER, SCISSORS, LIZARD, SPOCK)
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * @author – Xavier Maisel
+     * @param – String c_pick - the value which the computer picker,
+     *       String h_pick - the value which the human picker
+     * @return – boolean, returns true if the computer wins else returns false
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
